a5ff4b596151de6a0f412a9a73b67e1102fffbc7
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-24 16:34:24 +0530

    Create main.tf
    it creates the backup terraform state in the azure

---
+++ Infra/Terraform/bootstramp/main.tf
@@ -0,0 +1,21 @@
+provider azurerm{
+    features {
+      
+    }
+}
+resource "azurerm_resource_group" "tfstate" {
+  name = "rg-tfstate" //resourcegroup terraform state
+  location = "East US"
+}
+resource "azurerm_storage_account" "tfstate" {
+  name = "sadevsecopstfstate"
+  resource_group_name = azurerm_resource_group.tfstate.name
+  location = azurerm_resource_group.tfstate.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "tfstate" {
+  name = "tfstate"
+  storage_account_id = azurerm_storage_account.id
+  container_access_type = "private"
+}
