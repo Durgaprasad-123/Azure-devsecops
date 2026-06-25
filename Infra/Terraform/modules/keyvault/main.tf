af734908a8032af5748d4f106a85570e34294e26
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:30:38 +0530

    Refactor key vault name and add access policy
    Added a random string resource for key vault suffix and updated key vault name to use this suffix. Also added access policy for the current user with specified permissions.

--- Infra/Terraform/modules/keyvault/main.tf
+++ Infra/Terraform/modules/keyvault/main.tf
@@ -1,5 +1,11 @@
+resource "random_string" "kv_suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
 resource "azurerm_key_vault" "kv" {
-  name                = "devsecopskv2026xyz"
+  name                = "kv${random_string.kv_suffix.result}"
   location            = var.location
   resource_group_name = var.rg_name
 
@@ -8,4 +14,23 @@ resource "azurerm_key_vault" "kv" {
 
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
+
+  tags = {
+    Environment = "DevSecOps"
+    Project     = "Azure-DevSecOps"
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "current_user" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = var.tenant_id
+  object_id    = var.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover"
+  ]
 }
