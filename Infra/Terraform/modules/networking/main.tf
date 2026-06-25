3b51e7a0399e5ae24bd958d0f138c4aa95a2044a
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:22:39 +0530

    Fix subnet ID reference in networking module

--- Infra/Terraform/modules/networking/main.tf
+++ Infra/Terraform/modules/networking/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "nic"{
   resource_group_name = var.rg_name
   ip_configuration {
     name = "internal"
-    subnet_id = azurerm_subnet.subnet_id
+    subnet_id = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
     }
