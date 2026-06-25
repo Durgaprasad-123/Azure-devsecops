cfbb03c5f4520f3fb2f1141e7f9ba0c604df8428
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:26:18 +0530

    Generate ACR name with random suffix and add tags
    Added a random string resource to generate a suffix for the ACR name and updated the name to use this suffix. Added tags for environment and project.

--- Infra/Terraform/modules/acr/main.tf
+++ Infra/Terraform/modules/acr/main.tf
@@ -1,7 +1,19 @@
+resource "random_string" "acr_suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
 resource "azurerm_container_registry" "acr" {
-  name                = "acrdevsecops2026xyz"
+  name                = "acr${random_string.acr_suffix.result}"
   resource_group_name = var.rg_name
   location            = var.location
-  sku                 = "Basic"
-  admin_enabled       = false
+
+  sku           = "Basic"
+  admin_enabled = false
+
+  tags = {
+    Environment = "DevSecOps"
+    Project     = "Azure-DevSecOps"
+  }
 }
