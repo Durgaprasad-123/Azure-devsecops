7c3e00f25fe9d2aed0d811e3d4396b943b1bdcdc
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:17:48 +0530

    Change storage account name in backend.tf

--- Infra/Terraform/devsecops/backend.tf
+++ Infra/Terraform/devsecops/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "stdevsecopstfstate01"
+    storage_account_name = "sadevsecopstfstate01"
     container_name       = "tfstate"
     key                  = "devsecops.tfstate"
   }
