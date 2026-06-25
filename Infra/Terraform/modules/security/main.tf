539c57172ef0d138f88cf5a2d3996aac243ee4d2
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:24:57 +0530

    Refactor security rules in main.tf

--- Infra/Terraform/modules/security/main.tf
+++ Infra/Terraform/modules/security/main.tf
@@ -1,31 +1,31 @@
-resource "azurerm_network_security_group "nsg" {
+resource "azurerm_network_security_group" "nsg" {
   name = "devsecops-nsg"
   location = var.location
   resource_group_name = var.rg_name
 }
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+   security_rule {
+      name                       = "AllowSSH"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+    security_rule {
+      name                       = "AllowJenkins"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "8080"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
   }
-  security_rule {
-    name                       = "AllowJenkins"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8080"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
 
 resource "azurerm_subnet_network_security_group_association" "assoc" {
   subnet_id                 = var.subnet_id
