35286780a3ee46b6dafa3436340c55e82624e8d7
Pra-sad <68323713+Durgaprasad-123@users.noreply.github.com>
2026-06-25 13:35:54 +0530

    Add object_id to keyvault module configuration

--- Infra/Terraform/devsecops/main.tf
+++ Infra/Terraform/devsecops/main.tf
@@ -34,4 +34,5 @@ module "keyvault" {
   rg_name     = azurerm_resource_group.rg.name
   location    = azurerm_resource_group.rg.location
   tenant_id  = data.azurerm_client_config.current.tenant_id
+  object_id  = data.azurerm_client_config.current.object_id
 }
